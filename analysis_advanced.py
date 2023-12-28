08b41a136adde318eba8abe8426e529cb426f0d8
r-wata <ryusei.watanabe@fuji.waseda.jp>
2023-12-28 14:02:54 +0900

    Refactor analysis_advanced.py

--- analysis_advanced.py
+++ analysis_advanced.py
@@ -1,11 +1,42 @@
 import pandas as pd
 from linearmodels.panel import PanelOLS, RandomEffects, PooledOLS
+from linearmodels.panel.results import PanelEffectsResults
 from statsmodels.stats.diagnostic import het_breuschpagan
 
-from analysis_basic import calculate_vif
+
+def fe_analysis(df: pd.DataFrame, exog: list, dummy: dict[str, list], target: str = 'actual_employment_rate') -> PanelEffectsResults:
+    """
+    固定効果モデルによる分析
+    """
+    classcode_dummy = dummy['classcode']
+    year_dummy = dummy['year']
+    prefecture_dummy = dummy['prefecture']
+    formula = f"{target} ~ " + ' + '.join(exog) + \
+            ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in classcode_dummy]) + \
+            ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in year_dummy]) + \
+            ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in prefecture_dummy]) + ' + EntityEffects'
+    result = PanelOLS.from_formula(formula, df, check_rank=False, drop_absorbed=True).fit()
+    print(result)
+    return result
 
 
-def create_model_summary_and_hausman_test(df_original: pd.DataFrame):
+def re_analysis(df: pd.DataFrame, exog: list, dummy: dict[str, list], target: str = 'actual_employment_rate') -> PanelEffectsResults:
+    """
+    ランダム効果モデルによる分析
+    """
+    classcode_dummy = dummy['classcode']
+    year_dummy = dummy['year']
+    prefecture_dummy = dummy['prefecture']
+    formula = f"{target} ~ 1 +" + ' + '.join(exog) + \
+            ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in classcode_dummy]) + \
+            ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in year_dummy]) + \
+            ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in prefecture_dummy]) + ' + EntityEffects'
+    result = RandomEffects.from_formula(formula, df, check_rank=False).fit()
+    print(result)
+    return result
+
+
+def create_model_summary_and_hausman_test(df_original: pd.DataFrame, mode: str = 'fe'):
     """
     linearmodelsによる固定効果モデル分析
     ランダム効果モデルとしての固定効果モデル分析
@@ -13,45 +44,47 @@ def create_model_summary_and_hausman_test(df_original: pd.DataFrame):
     """
     # 説明変数の定義
     exog = ['total_regular_worker'] + [col for col in df_original.columns if 'major_class_code_' in col or 'prefecture_' in col or 'year_' in col] \
-        + ['aging_rate', 'density', 'city_population_rate', 'cpi_regional_diff', 'pref_gdp']
+        + ['aging_rate', 'density', 'city_population_rate', 'cpi_regional_diff']
     classcode_dummy = [col for col in df_original.columns if 'major_class_code_' in col]
     year_dummy = [col for col in df_original.columns if 'year_' in col]
     prefecture_dummy = [col for col in df_original.columns if 'prefecture_' in col]
     df = df_original[exog + ['actual_employment_rate', 'id', 'year']]
     df = df.set_index(['id', 'year'])
 
-    # print("多重共線性チェック")
-    # result = calculate_vif(df)
-    # print(f"多重共線性により削除された変数：{len(result)}")
-    # print(result)
-    # print("多重共線性チェック終了")
+    if mode == 'fe':
+        fe_analysis(df, exog, {'classcode': classcode_dummy, 'year': year_dummy, 'prefecture': prefecture_dummy})
+    # ランダム効果モデル
+    elif mode == 're':
+        re_analysis(df, exog, {'classcode': classcode_dummy, 'year': year_dummy, 'prefecture': prefecture_dummy})
+    elif mode == 'fe_re':
+        result_fe = fe_analysis(df, exog, {'classcode': classcode_dummy, 'year': year_dummy, 'prefecture': prefecture_dummy})
+        result_re = re_analysis(df, exog, {'classcode': classcode_dummy, 'year': year_dummy, 'prefecture': prefecture_dummy})
+        residuals_fe = result_fe.resids
+        residuals_re = result_re.resids
+        bp_test = het_breuschpagan(residuals_fe - residuals_re, result_re.model.exog.dataframe)
+        labels = ['LM Statistic', 'LM-Test p-value', 'F-Statistic', 'F-Test p-value']
+        print("ハウスマン検定")
+        print(dict(zip(labels, bp_test)))
+    elif mode == 'fe_extract':
+        result_fe = fe_analysis(df, exog, {'classcode': classcode_dummy, 'year': year_dummy, 'prefecture': prefecture_dummy})
+        extract_effect_and_reg_analysis(df, result_fe, exog, {'classcode': classcode_dummy, 'year': year_dummy, 'prefecture': prefecture_dummy})
 
-    # 固定効果モデル
-    formula_fe = 'actual_employment_rate ~ ' + ' + '.join(exog) + \
+def extract_effect_and_reg_analysis(df: pd.DataFrame, results: PanelEffectsResults, exog: list, dummy: dict[str, list]):
+    """
+    固定効果モデルの結果から、効果量と回帰分析を行う
+    """
+    fe = results.estimated_effects
+    df['estimated_effects'] = fe.values
+    classcode_dummy = dummy['classcode']
+    year_dummy = dummy['year']
+    prefecture_dummy = dummy['prefecture']
+    formula = 'estimated_effects ~ actual_employment_rate + ' + ' + '.join(exog) + \
         ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in classcode_dummy]) + \
         ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in year_dummy]) + \
         ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in prefecture_dummy]) + ' + EntityEffects'
-    result_fe = PanelOLS.from_formula(formula_fe, df, check_rank=False, drop_absorbed=True).fit()
-    print(result_fe)
+    result = PanelOLS.from_formula(formula, df, check_rank=False, drop_absorbed=True).fit()
 
-    # ランダム効果モデル
-    formula_re = 'actual_employment_rate ~ 1 +' + ' + '.join(exog) + \
-        ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in classcode_dummy]) + \
-        ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in year_dummy]) + \
-        ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in prefecture_dummy])
-    result_re = RandomEffects.from_formula(formula_re, df, check_rank=False).fit()
-    print(result_re)
-
-    # ハウスマン検定1
-    bp_test = het_breuschpagan(result_re.resids, result_re.model.exog.dataframe)
-    labels = ['LM Statistic', 'LM-Test p-value', 'F-Statistic', 'F-Test p-value']
-    print("ハウスマン検定1")
-    print(dict(zip(labels, bp_test)))
-
-    # ハウスマン検定2
-    # restriction = 'total_regular_worker = ' + ' = '.join(classcode_dummy) + ' = ' + ' = '.join(year_dummy) + ' = 0'
-    # print("ハウスマン検定2")
-    # print(result_re.wald_test(formula=restriction))
+    print(result)
 
 
 def perform_pooled_ols(df: pd.DataFrame, target_year: int):
@@ -59,20 +92,19 @@ def perform_pooled_ols(df: pd.DataFrame, target_year: int):
     PooledOLSを実行する関数
     """
     exog = ['total_regular_worker'] + [col for col in df.columns if 'major_class_code_' in col or 'prefecture_' in col] \
-        + ['aging_rate', 'density', 'city_population_rate', 'cpi_regional_diff', 'pref_gdp']
+        + ['aging_rate', 'density', 'city_population_rate', 'cpi_regional_diff']
     df = df[df['year'] == target_year]
-    df = df.set_index(['id'])
-    df = df[exog + ['actual_employment_rate', 'year']]
-    print(df.head())
+    df = df.set_index(['id', 'year'])
+    df = df[exog + ['actual_employment_rate']]
     classcode_dummy = [col for col in df.columns if 'major_class_code_' in col]
     prefecture_dummy = [col for col in df.columns if 'prefecture_' in col]
     formula_pooled = 'actual_employment_rate ~ 1 + ' + ' + '.join(exog) + \
         ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in classcode_dummy]) + \
         ' + ' + ' + '.join([f'total_regular_worker*{dummy}' for dummy in prefecture_dummy])
-    result_pooled = PooledOLS.from_formula(formula_pooled, df).fit()
-    
+    result_pooled = PooledOLS.from_formula(formula_pooled, df, check_rank=False).fit()
     print(f"Pooled OLS: {target_year}")
     print(result_pooled)
+    
 
 
 def add_external_data(df: pd.DataFrame, path: str, new_column_name: str):
@@ -106,17 +138,18 @@ def filter_by_year_and_remove_unused_dummy(df: pd.DataFrame, years: list) -> pd.
 
 def main():
     # create_extended_dummy()
-    target_year = [2014, 2015, 2016, 2017, 2018, 2019, 2020]
+    # target_year = [2014, 2015, 2016, 2017, 2018, 2019, 2020]
 
     # 固定効果モデルによる推定
-    df = pd.read_csv('datasets/dummy_extended_2.csv')
+    df = pd.read_csv('datasets/dummy_extended.csv')
+    create_model_summary_and_hausman_test(df, mode='fe_extract')
+
     # df = filter_by_year_and_remove_unused_dummy(df, target_year)
     # df = add_external_data(df, 'datasets/pref_gdp.csv', 'pref_gdp')
     # df.to_csv('datasets/dummy_extended_2.csv', index=False)
     
-    # create_model_summary_and_hausman_test(df)
-    for year in target_year:
-        perform_pooled_ols(df, year)
+    # for year in target_year:
+    #     perform_pooled_ols(df, year)
 
 
 
