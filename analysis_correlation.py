11eff3741f29fdcaf1e299aa889d6f0bdd9a0f36
r-wata <ryusei.watanabe@fuji.waseda.jp>
2024-01-06 16:30:37 +0900

    Refactor code to convert prefecture names in CSV file

---
+++ analysis_correlation.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import seaborn as sns
+
+
+def calculate_specialization(df):
+    total_all_prefectures = df.loc[df['pref'] == 'all_prefectures'].drop(['pref', '小計'], axis=1)
+    df = df.loc[df['pref'] != 'all_prefectures']
+    
+    for column in df.columns:
+        if column not in ['pref', '小計']:
+            df[f'{column}_特化係数'] = (df[column] / df['小計']) / (total_all_prefectures[column].values[0] / total_all_prefectures.sum(axis=1).values[0])
+    
+    return df
+
+
+def extract_actual_employment_rate(df, year: int) -> pd.DataFrame:
+    df = df[['pref', str(year)]]
+    df = df.rename(columns={str(year): '都道府県実雇用率'})
+    return df
+
+
+def merge_dfs_on_pref(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    merged_df = pd.merge(df1, df2, on='pref')
+    print(merged_df.columns)
+    return merged_df
+
+
+def plot_correlation(df: pd.DataFrame, save_path: str = None):
+    num_columns = len([col for col in df.columns if col.endswith('_特化係数')])
+    num_rows = num_columns // 2 if num_columns % 2 == 0 else (num_columns // 2) + 1
+    fig, axes = plt.subplots(nrows=num_rows, ncols=2, figsize=(20, num_rows * 5))  # サイズを調整
+    axes = axes.flatten()
+    
+    for ax, column in zip(axes, [col for col in df.columns if col.endswith('_特化係数')]):
+        sns.scatterplot(data=df, x='都道府県実雇用率', y=column, ax=ax)
+        ax.set_title(f'都道府県実雇用率と{column}の相関', fontsize=10)  # フォントサイズを調整
+        ax.set_xlabel('都道府県実雇用率', fontsize=8)
+        ax.set_ylabel(column, fontsize=8)
+        
+        correlation = df['都道府県実雇用率'].corr(df[column])
+        ax.text(0.05, 0.95, f'相関係数: {correlation:.2f}', transform=ax.transAxes, fontsize=8, verticalalignment='top')
+    
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.4, hspace=0.6)  # スペースを調整
+    if save_path:
+        plt.savefig(save_path, format='pdf')
+    else:
+        plt.show()
+
+
+def main():
+    data_path = 'datasets/category_pref_gdp.csv'
+    df = pd.read_csv(data_path)
+    df = calculate_specialization(df)
+    df.to_csv('datasets/category_pref_gdp_specialization.csv', index=False)
+
+    aer_path = 'datasets/density_etc/pref_actual_employment_rate.csv'
+    aer_df = pd.read_csv(aer_path)
+    aer_df = extract_actual_employment_rate(aer_df, 2020)
+
+    merged_df = merge_dfs_on_pref(df, aer_df)
+    plot_correlation(merged_df, save_path='datasets/correlation.pdf')
+
+
+if __name__ == '__main__':
+    main()
--- convert_xlsx_to_csv.py
+++ convert_xlsx_to_csv.py
@@ -1,20 +1,21 @@
 import pandas as pd
 
-def convert_xlsx_to_csv(path, sheet_name, output_path, header_row=0, index_col=0, nrows=None, drop_columns=[]):
-    # 都道府県名の英語表記
-    prefecture_dict = {
-        '北海道': 'hokkaido', '青森県': 'aomori', '岩手県': 'iwate', '宮城県': 'miyagi', '秋田県': 'akita',
-        '山形県': 'yamagata', '福島県': 'fukushima', '茨城県': 'ibaraki', '栃木県': 'tochigi', '群馬県': 'gunma',
-        '埼玉県': 'saitama', '千葉県': 'chiba', '東京都': 'tokyo', '神奈川県': 'kanagawa', '新潟県': 'niigata',
-        '富山県': 'toyama', '石川県': 'ishikawa', '福井県': 'fukui', '山梨県': 'yamanashi', '長野県': 'nagano',
-        '岐阜県': 'gifu', '静岡県': 'shizuoka', '愛知県': 'aichi', '三重県': 'mie', '滋賀県': 'shiga',
-        '京都府': 'kyoto', '大阪府': 'osaka', '兵庫県': 'hyogo', '奈良県': 'nara', '和歌山県': 'wakayama',
-        '鳥取県': 'tottori', '島根県': 'shimane', '岡山県': 'okayama', '広島県': 'hiroshima', '山口県': 'yamaguchi',
-        '徳島県': 'tokushima', '香川県': 'kagawa', '愛媛県': 'ehime', '高知県': 'kochi', '福岡県': 'fukuoka',
-        '佐賀県': 'saga', '長崎県': 'nagasaki', '熊本県': 'kumamoto', '大分県': 'oita', '宮崎県': 'miyazaki',
-        '鹿児島県': 'kagoshima', '沖縄県': 'okinawa'
-    }
 
+# 都道府県名の英語表記
+prefecture_dict = {
+    '北海道': 'hokkaido', '青森県': 'aomori', '岩手県': 'iwate', '宮城県': 'miyagi', '秋田県': 'akita',
+    '山形県': 'yamagata', '福島県': 'fukushima', '茨城県': 'ibaraki', '栃木県': 'tochigi', '群馬県': 'gunma',
+    '埼玉県': 'saitama', '千葉県': 'chiba', '東京都': 'tokyo', '神奈川県': 'kanagawa', '新潟県': 'niigata',
+    '富山県': 'toyama', '石川県': 'ishikawa', '福井県': 'fukui', '山梨県': 'yamanashi', '長野県': 'nagano',
+    '岐阜県': 'gifu', '静岡県': 'shizuoka', '愛知県': 'aichi', '三重県': 'mie', '滋賀県': 'shiga',
+    '京都府': 'kyoto', '大阪府': 'osaka', '兵庫県': 'hyogo', '奈良県': 'nara', '和歌山県': 'wakayama',
+    '鳥取県': 'tottori', '島根県': 'shimane', '岡山県': 'okayama', '広島県': 'hiroshima', '山口県': 'yamaguchi',
+    '徳島県': 'tokushima', '香川県': 'kagawa', '愛媛県': 'ehime', '高知県': 'kochi', '福岡県': 'fukuoka',
+    '佐賀県': 'saga', '長崎県': 'nagasaki', '熊本県': 'kumamoto', '大分県': 'oita', '宮崎県': 'miyazaki',
+    '鹿児島県': 'kagoshima', '沖縄県': 'okinawa', '全県計': 'all_prefectures'
+}
+
+def convert_xlsx_to_csv(path, sheet_name, output_path, header_row=0, index_col=0, nrows=None, drop_columns=[]):
     # エクセルファイルの読み込み
     df = pd.read_excel(path, sheet_name=sheet_name, header=header_row, index_col=index_col, nrows=nrows)
 
@@ -33,15 +34,24 @@ def remove_last_column_from_csv(path):
     df.to_csv(path, index=False)
 
 
+def convert_prefecture_name_in_csv(path):
+    df = pd.read_csv(path, encoding='shift-jis')
+    df.rename(columns={df.columns[0]: 'pref'}, inplace=True)
+    df['pref'] = df['pref'].map(prefecture_dict)
+    df.to_csv(path, index=False)
+
+
+
 def main():
-    dataset_path = 'datasets/gdp.xlsx'
-    sheet_name_1 = '実数'
+    dataset_path = 'datasets/density_etc_data.xlsx'
+    convert_prefecture_name_in_csv('datasets/category_pref_gdp.csv')
+    # sheet_name_1 = '都道府県実雇用率'
     # sheet_name_2 = 'density'
     # sheet_name_3 = 'city_population_rate'
     # sheet_name_4 = 'cpi_regional_diff'
     drop_columns = ['Unnamed: 0', 'Unnamed: 2', 'Unnamed: 13']
 
-    convert_xlsx_to_csv(dataset_path, sheet_name_1, 'datasets/pref_gdp.csv', header_row=4, index_col=1, nrows=48, drop_columns=drop_columns)
+    # convert_xlsx_to_csv(dataset_path, sheet_name_1, 'datasets/density_etc/pref_actual_employment_rate.csv')
     # convert_xlsx_to_csv(dataset_path, sheet_name_2, 'datasets/density_etc/density.csv')
     # convert_xlsx_to_csv(dataset_path, sheet_name_3, 'datasets/density_etc/city_population_rate.csv')
     # convert_xlsx_to_csv(dataset_path, sheet_name_4, 'datasets/density_etc/cpi_regional_diff.csv')
